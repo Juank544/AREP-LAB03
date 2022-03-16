ddc6fc0af63db894343fdce335655c155ccf418c
Juank544 <camilo.gv@live.com>
2022-03-16 01:38:04 -0500

    ajustes - obtener últimas 10 cadenas

--- src/main/java/co/edu/escuelaing/services/MongoDB.java
+++ src/main/java/co/edu/escuelaing/services/MongoDB.java
@@ -1,9 +1,12 @@
 package co.edu.escuelaing.services;
 
 import com.mongodb.client.*;
+import com.mongodb.client.model.Sorts;
 import org.bson.Document;
 
 import java.util.*;
+
+import static com.mongodb.client.model.Aggregates.limit;
 
 public class MongoDB {
     //Yo sé que esto no se debe hacer, pero es con fines académicos xd
@@ -21,8 +24,12 @@ public class MongoDB {
     public List<String> getCadenas(){
         List<String> cadenasList = new ArrayList<>();
 
-        FindIterable<Document> findIterable =cadenasCollection.find().limit(10);
-        Iterator<Document> iterator = findIterable.iterator();
+        FindIterable<Document> findIterable =cadenasCollection.find().sort(Sorts.descending("fecha")).limit(-10);
+
+//        Iterator<Document> iterator = findIterable.iterator();
+//        while (iterator.hasNext()){
+//            System.out.println(iterator.next());
+//        }
 
         ArrayList<Document> temp = new ArrayList<>();
         findIterable.into(temp);
